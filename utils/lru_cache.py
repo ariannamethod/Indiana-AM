eb18fd383c94953874e0792b06c6c1bcc7c51395
Arianna Method <theariannamethod@gmail.com>
2025-08-14 06:59:23 +0300

    feat: add ttl to lru cache

--- utils/lru_cache.py
+++ utils/lru_cache.py
@@ -1,15 +1,21 @@
-from collections import OrderedDict
 import time
+from collections import OrderedDict
 
 
 class LRUCache:
     """Simple LRU cache with max length and TTL support."""
 
-    def __init__(self, maxlen: int = 1024):
+    def __init__(self, maxlen: int = 1024, ttl: float | None = None):
         self.maxlen = maxlen
+        self.ttl = ttl
         self._data: OrderedDict[str, tuple[str, float]] = OrderedDict()
 
     def get(self, key: str, default=None):
+        if self.ttl is not None:
+            cutoff = time.time() - self.ttl
+            keys = [k for k, (_, ts) in self._data.items() if ts < cutoff]
+            for k in keys:
+                del self._data[k]
         item = self._data.get(key)
         if item is None:
             return default
